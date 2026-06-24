8e421cc5dbf991fc755d54813451ad792623dd05
Steven Cassamajor <contributor@networkaxis.org>
2026-06-24 01:51:14 -0400

    Separate read_files tool from main.go

--- main.go
+++ main.go
@@ -19,10 +19,6 @@ const (
 	TOOL   string = "\x1b[92mtool\x1b[0m: %s(%s)\n" // Colorize the text "tool"
 )
 
-type ReadFileInput struct {
-	Path string `json:"path" jsonschema_description:"The relative path of a file in the working directory."`
-}
-
 func GenerateSchema[T any]() anthropic.ToolInputSchemaParam {
 	reflector := jsonschema.Reflector{
 		AllowAdditionalProperties: false,
@@ -36,29 +32,6 @@ func GenerateSchema[T any]() anthropic.ToolInputSchemaParam {
 	return anthropic.ToolInputSchemaParam{
 		Properties: schema.Properties,
 	}
-}
-
-func ReadFile(input json.RawMessage) (string, error) {
-	readFileInput := ReadFileInput{}
-	err := json.Unmarshal(input, &readFileInput)
-	if err != nil {
-		panic(err)
-	}
-
-	content, err := os.ReadFile(readFileInput.Path)
-	if err != nil {
-		return "", err
-	}
-	return string(content), nil
-}
-
-var ReadFileInputSchema = GenerateSchema[ReadFileInput]()
-
-var ReadFileDefinition = ToolDefinition{
-	Name:        "read_file",
-	Description: "Read the contents of a given relative file path. Use this when you want to see what is inside a file. Do not use this with directory names.",
-	InputSchema: ReadFileInputSchema,
-	Function:    ReadFile,
 }
 
 type Agent struct {
